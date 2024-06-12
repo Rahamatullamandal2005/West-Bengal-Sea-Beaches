451b914da3f2c3c5a59a0fbef74670239ec03377
Rahamatullamandal2005 <rahamatmandal2005@gmail.com>
2024-06-12 12:53:58 +0530

    Updated all file

---
+++ Java Script/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+var nav = document.querySelector(".nav");
+
+function openmenu() {
+    nav.style.right = "0";
+}
+
+function closemenu() {
+    nav.style.right = "-200px";
+}
---
+++ Java Script/scrol.js
@@ -0,0 +1,8 @@
+// Assuming the element has the ID "myElement" (replace with actual element)
+if (document.getElementById("myElement")) {
+  const elementTop = document.getElementById("myElement").offsetTop;
+  // Use elementTop here
+} else {
+  // Handle the case where the element is not found
+  console.log("Element with ID 'myElement' not found");
+}
